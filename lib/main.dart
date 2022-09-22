a36ec41597c0b804b64bc2f68cb4b59716c08ca7
ttoghere <tuncsoftwarehere@gmail.com>
2022-09-22 12:03:38 +0300

    will implement to design

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ghealth/api_test.dart';
 import 'package:ghealth/monitor_dashboard_screen.dart';
 
 void main() => runApp( MyApp());
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: MonitorDashboard()
+      home: HealthApp()
     );
   }
 }
